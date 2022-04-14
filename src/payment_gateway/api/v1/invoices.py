61b3f0cd7cbc8ff518e8ddbb9bfb17c884708ccc
alisovenko <alexander.lisovenko@gmail.com>
2022-04-14 23:29:57 +0300

    feat: update invoice create api - add product info

--- src/payment_gateway/api/v1/invoices.py
+++ src/payment_gateway/api/v1/invoices.py
@@ -43,6 +43,21 @@ async def get_invoices(db: AsyncSession = Depends(get_session)):
     ]
 
 
+class InvoiceRequest(BaseModel):
+    product_name: str = 'subscription, 1 month'
+    product_count: int = 2
+    product_price_currency: str = 'RUB'
+    product_price_amount_total: float = 600
+
+    def to_db_model(self):
+        return Invoice(
+            product_name=self.product_name,
+            product_count=self.product_count,
+            product_price_currency=self.product_price_currency,
+            product_price_amount_total=self.product_price_amount_total,
+        )
+
+
 class ResponseInvoiceWithCheckout(BaseModel):
     invoice: ResponseInvoice
     checkout_url: str
@@ -53,10 +68,10 @@ class ResponseInvoiceWithCheckout(BaseModel):
     summary='Create invoice',
     response_model=ResponseInvoiceWithCheckout,
 )
-async def create_invoice(db: AsyncSession = Depends(get_session)):
+async def create_invoice(invoice_request: InvoiceRequest, db: AsyncSession = Depends(get_session)):
     payment_system = get_payment_system()
 
-    invoice = Invoice()
+    invoice = invoice_request.to_db_model()
 
     checkout_info = await payment_system.create_checkout(invoice)
 
