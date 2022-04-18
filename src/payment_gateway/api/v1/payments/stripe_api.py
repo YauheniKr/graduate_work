ab899430b873dd18e775ad0db0ff0231085e44dd
johanijbabaj <khaidukov.eugeniy@gmail.com>
2022-04-19 01:15:08 +0300

    fix errors with isort,flake8

--- src/payment_gateway/api/v1/payments/stripe_api.py
+++ src/payment_gateway/api/v1/payments/stripe_api.py
@@ -1,11 +1,14 @@
 import json
+import logging
+
 import stripe
-from fastapi import APIRouter, Depends, status, Request
-from fastapi.responses import JSONResponse
 from core.settings import settings
+from fastapi import APIRouter, Request, status
+from fastapi.responses import JSONResponse
 
 stripe.api_key = settings.api_key
 endpoint_secret = settings.api_webhook_key
+logger = logging.getLogger('paygateway.api.v1.stripe')
 
 router = APIRouter()
 
@@ -13,42 +16,48 @@ router = APIRouter()
 @router.post('/webhook', summary='Stripe webhook')
 async def webhook(request: Request):
     event = None
-    data = await request.body()
+    request_data = await request.body()
     try:
-        event = json.loads(data)
-    except Exception as e:
-        print('⚠️  Webhook error while parsing basic request.' + str(e))
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
-                            content={'message': '⚠️  Webhook error while parsing basic request.' + str(e)})
+        event = json.loads(request_data)
+    except Exception as exc:
+        logger.error(f'⚠️  Webhook error while parsing request. {exc}')
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                'message': f'⚠️  Webhook error while parsing request. {exc}',
+            },
+        )
     if endpoint_secret:
         # Only verify the event if there is an endpoint secret defined
         # Otherwise use the basic event deserialized with json
         sig_header = request.headers.get('stripe-signature')
         try:
             event = stripe.Webhook.construct_event(
-                data, sig_header, endpoint_secret
+                request_data, sig_header, endpoint_secret,
             )
-        except stripe.error.SignatureVerificationError as e:
-            print('⚠️  Webhook signature verification failed.' + str(e))
-            return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
-                                content={'message': '⚠️  Webhook signature verification failed.'})
+        except stripe.error.SignatureVerificationError as exc:
+            logger.error(f'⚠️  Webhook signature verification failed. {exc}')
+            return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content={
+                    'message': f'⚠️ Webhook signature failed. {exc}',
+                },
+            )
 
     # Handle the event
     if event and event['type'] == 'payment_intent.succeeded':
-        payment_intent = event['data']['object']  # contains a stripe.PaymentIntent
-        print('Payment for {} succeeded'.format(payment_intent['amount']))
-        # Then define and call a method to handle the successful payment intent.
-        # handle_payment_intent_succeeded(payment_intent)
+        payment_intent = event['data']['object']
+        logger.debug(f'Payment for {payment_intent["amount"]} succeeded')
     elif event['type'] == 'checkout.session.completed':
-        checkout_session = event['data']['object']  # contains a stripe.PaymentMethod
-        print('✅ Payment session for {} succeeded'.format(checkout_session['amount_total']))
-    elif event['type'] == 'payment_method.attached':
-        payment_method = event['data']['object']  # contains a stripe.PaymentMethod
-        # Then define and call a method to handle the successful attachment of a PaymentMethod.
-        # handle_payment_method_attached(payment_method)
+        checkout_session = event['data']['object']
+        sum_amount = checkout_session['amount_total']
+        logger.debug(
+            f'✅ Payment session for {sum_amount} succeeded',
+        )
     else:
         # Unexpected event type
-        print('Unhandled event type {}'.format(event['type']))
-
-    return JSONResponse(status_code=status.HTTP_200_OK,
-                        content={'message': '⚠️  Ammm. Ok'})
+        logger.debug(f'Unhandled event type {event["type"]}')
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={'message': '⚠️  Ammm. Ok'},
+    )
