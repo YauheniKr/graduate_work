62d33ce73fe7534f7296298567a594de7ad36e76
alisovenko <alexander.lisovenko@gmail.com>
2022-04-21 12:33:49 +0300

    fix: pass success and cancel urls into stripe

--- src/payment_gateway/services/payment_systems/stripe_service.py
+++ src/payment_gateway/services/payment_systems/stripe_service.py
@@ -40,8 +40,8 @@ class StripePaymentSystem(PaymentSystem):
                 line_items=checkout_line_items,
                 mode='payment',
                 # FIXME: изменить ссылка на указанные в ручке
-                success_url='http://localhost:8001/success.html',
-                cancel_url='http://localhost:8001/cancel.html',
+                success_url=success_url,
+                cancel_url=cancel_url,
             )
         except Exception as exc:
             logger.error(f'Checkout session error: {exc}')
